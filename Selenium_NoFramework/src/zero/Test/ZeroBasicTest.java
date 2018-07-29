993309d6a3553fbb4ef512279197b223fbdb884f
phanigithub <phani.rkt@gmail.com>
2018-07-29 15:18:32 +0530

    # WARNING: head commit changed in the meantime
    Override everything

---
+++ Selenium_NoFramework/src/zero/Test/ZeroBasicTest.java
@@ -0,0 +1,10 @@
+package zero.Test;
+
+public class ZeroBasicTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Selenium_NoFramework/src/zero/Test/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Krishnaa
+ *
+ */
+package zero.Test;
