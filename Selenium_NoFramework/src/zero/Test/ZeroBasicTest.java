354d7d84dc17504e2360cd8f2297f181f1d0b629
phanigithub <phani.rkt@gmail.com>
2018-07-31 02:21:25 +0530

    COMMENT

--- Selenium_NoFramework/src/zero/Test/ZeroBasicTest.java
+++ Selenium_NoFramework/src/zero/Test/ZeroBasicTest.java
@@ -4,7 +4,7 @@ public class ZeroBasicTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Zero Base Test.");
+		//System.out.println("Comment");
 			
 	}
 
